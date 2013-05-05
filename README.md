1053908158bf958a26faabb0a0d1f4fc11dfd4d5
codeeffect <steve@codeeffect.co.uk>
2013-05-05 19:06:56 +0100

    First commit of find selected

---
+++ README.md
@@ -0,0 +1,55 @@
+# Find Selected #
+
+A very simple additional pair of methods for quickly searching for text within a
+page. Sublime Text 2 (http://www.sublimetext.com/2).
+
+Two additional search methods are available:
+
+**find\_selected\_next** - If some text is selected then it is searched for and 
+  the cursor highlights the next occurrence of the text. If no text is selected 
+  then depending on the passed argument we either search for the last *Find* 
+  search term ("last_search") or the clipboard ("clipboard").
+
+**find\_selected\_previous** - As above but we search backwards
+
+This package doesn't really add much in the way of new functionality, it just makes 
+available existing functionality under one method or keypress. Previously to 
+search for the last search term you would call the "find_next" method or to search 
+for selected text you would call the "find_under" method. Now you can just call 
+the "find_selected" method and depending on whether text is selected or not the 
+appropriate built-in method is called for you. It's a somewhat trivial change in 
+behaviour but one I found that I missed greatly when migrating from UltraEdit. 
+There is also the added ability to search for whatever is on the clipboard in one 
+keypress.
+
+**Important note about key bindings:** If you have SublimeCodeIntel installed it 
+grabs CTRL+F3 by default. If you wish to override this behaviour you will need to
+put Find Selected keys in your user key bindings file. To do this open the default 
+key bindings at "Preferences -> Package settings -> Find Selected -> Key Bindings 
+- Default". Copy and paste these into your user file at "Preferences -> Key 
+Bindings - User" ensuring you add the trailing comma as appropriate.
+
+
+## License ##
+
+Find selected is licensed under the MIT license.
+
+  Copyright (c) 2013 Steven Perfect <steve@codeeffect.co.uk>
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in
+  all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+  THE SOFTWARE.
---
+++ findselected.py
@@ -0,0 +1,162 @@
+import sublime
+import sublime_plugin
+
+
+def removeNewlinesAndCut(text):
+    if "\n" in text:
+        text = text[0:text.find("\n")] + '...'
+    return text
+
+
+class FindSelectedNextCommand(sublime_plugin.TextCommand):
+    def run(self, edit, alt="last_search"):
+        """Find next instance of selected text.
+
+If selection empty then look for the last Find command search term or the clipboard depending on the passed argument.
+
+"""
+        view = self.view
+        # Get the first highlighted string to search for
+        selected = view.sel()[0]
+        # Note if we're searching for the clipboard or a selection
+        selection = True
+
+        # If start and end cursor points are different then we have some text selected
+        if selected.a != selected.b:
+            # We don't call "find_under" here as it populates the find panel and
+            # so we would lose its value in case we wanted to use it later
+            # Get the text of the selection
+            searchText = view.substr(sublime.Region(selected.a, selected.b))
+            # If we've selected right to left then ensure that we use the higher
+            # of the two for comparison
+            if selected.a < selected.b:
+                selectedPos = selected.b
+            else:
+                selectedPos = selected.a
+        elif alt == "last_search":
+            # Run the built in find_next command from the window context
+            return view.window().run_command("find_next")
+        elif alt == "clipboard":
+            selection = False
+            # Search for what's on the clipboard
+            searchText = sublime.get_clipboard()
+            selectedPos = selected.a
+
+        # Find the next instance of our search string after the cursor position
+        match = view.find(searchText, selectedPos, sublime.LITERAL | sublime.IGNORECASE)
+
+        # If nothing found underneath our cursor position
+        if not match:
+            # Try from the top of the page
+            match = view.find(searchText, 0, sublime.LITERAL | sublime.IGNORECASE)
+
+        # If still nothing found
+        if not match:
+            return sublime.status_message('''Unable to find "%s"''' % removeNewlinesAndCut(searchText))
+
+        # If we only find our current selection then message to say only 1 found
+        if selection and match.b == selectedPos:
+            return sublime.status_message('''Only 1 instance of "%s" found''' % removeNewlinesAndCut(searchText))
+
+        # Highlight it
+        view.sel().clear()
+        view.sel().add(match)
+        # Scroll to it
+        view.show(match)
+        # Zoidberg eyeball, iris out!
+        return sublime.status_message('''Found "%s"''' % removeNewlinesAndCut(searchText))
+
+
+class FindSelectedPreviousCommand(sublime_plugin.TextCommand):
+    def run(self, edit, alt="last_search"):
+        """Find previous instance of selected text.
+
+If selection empty then look for the last Find command search term or the clipboard depending on the passed argument.
+
+"""
+        view = self.view
+        # Get the first highlighted string to search for
+        selected = view.sel()[0]
+        # Note if we're searching for the clipboard or a selection
+        selection = True
+
+        # If start and end cursor points are different then we have some text selected
+        if selected.a != selected.b:
+            # We don't call "find_under_previous" here as it populates the find
+            # panel and so we would lose its value in case we wanted to use it later
+            # Get the text of the selection
+            searchText = view.substr(sublime.Region(selected.a, selected.b))
+            # If we've selected right to left then ensure that we use the higher
+            # of the two for comparison
+            if selected.a > selected.b:
+                selectedPos = selected.a
+            else:
+                selectedPos = selected.b
+        elif alt == "last_search":
+            # Run the built in find_prev command from the window context
+            return view.window().run_command("find_prev")
+        elif alt == "clipboard":
+            # Search for what's on the clipboard
+            searchText = sublime.get_clipboard()
+            selectedPos = selected.b
+            selection = False
+
+        # Find all instances of our search string in order
+        matches = view.find_all(searchText, sublime.LITERAL | sublime.IGNORECASE)
+
+        # If we're searching for selected text
+        if selection:
+            # If we only find one match
+            if len(matches) == 1:
+                return sublime.status_message('''Only 1 instance of "%s" found''' % removeNewlinesAndCut(searchText))
+
+            # If our selected text is the first found then we need the last found
+            if matches[0].b == selectedPos:
+                match = matches[-1]
+            else:
+                # Otherwise loop them until we reach our selection
+                for possibleMatch in matches:
+                    # We want the one before
+                    if possibleMatch.b == selectedPos:
+                        break
+                    match = possibleMatch
+
+        # Else we're searching for a text string
+        else:
+            # If not found
+            if not matches:
+                return sublime.status_message('''Unable to find "%s"''' % removeNewlinesAndCut(searchText))
+            # If only 1 found
+            if len(matches) == 1:
+                match = matches[0]
+            # Else find the one closest above our cursor
+            else:
+                # Work out the position of our cursor for later comparison
+                (row, col) = view.rowcol(selectedPos)
+                cursorPostition = view.text_point(row, col)
+                # Work out the position of the first match
+                (row, col) = view.rowcol(matches[0].b)
+                possibleCursorMatch = view.text_point(row, col)
+                # If the first one is further down the page than our cursor
+                if cursorPostition < possibleCursorMatch:
+                    # Then go to the last one
+                    match = matches[-1]
+                else:
+                    # Nothing left but to loop all matches and calculate their positions
+                    for possibleMatch in matches:
+                        (row, col) = view.rowcol(possibleMatch.a)
+                        possibleCursorMatch = view.text_point(row, col)
+                        # We want the one before our cursor
+                        if possibleCursorMatch < cursorPostition:
+                            match = possibleMatch
+                        else:
+                            # If we match below our cursor position then stop searching
+                            break
+
+        # Highlight it
+        view.sel().clear()
+        view.sel().add(match)
+        # Scroll to it
+        view.show(match)
+        # Rubber baby buggy bumpers.
+        return sublime.status_message('''Found "%s"''' % removeNewlinesAndCut(searchText))
